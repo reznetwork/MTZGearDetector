9fca29bf559e87d31e376a63c2d663676030fc0e
reznetwork <alexey.lavrentiev@gmail.com>
2025-11-29 14:02:05 +0300

    Fix ring buffer declarations order

--- main.ino
+++ main.ino
@@ -461,17 +461,14 @@ void maintainWifi(){
 }
 
 // ---------------- Logging ring buffer ----------------
+struct LogEntry { uint32_t t; uint16_t code; };
+constexpr size_t LOG_SIZE=256; LogEntry logBuf[LOG_SIZE]; size_t logHead=0;
+
 void logGear(uint16_t g){
-  extern LogEntry logBuf[];
-  extern size_t logHead;
   logBuf[logHead].t = millis();
   logBuf[logHead].code = g;
   logHead = (logHead+1) % LOG_SIZE;
 }
-
-// Ring buffer storage
-struct LogEntry { uint32_t t; uint16_t code; };
-constexpr size_t LOG_SIZE=256; LogEntry logBuf[LOG_SIZE]; size_t logHead=0;
 
 // heuristics
 inline bool magOK(uint16_t mag) { return (mag >= 1000 && mag <= 3500); }
